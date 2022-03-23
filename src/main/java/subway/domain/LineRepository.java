7ee556a70351b2eeb56bf5f2edcde56759b34ad8
eunbii0213 <eunbii0213@naver.com>
2022-03-23 15:17:46 +0900

    feat: feature1

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -8,6 +8,31 @@ import java.util.Objects;
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
+    public LineRepository(){
+        lines.add(new Line("2호선"));
+        lines.add(new Line("3호선"));
+        lines.add(new Line("신분당선"));
+
+        Line line = lines.get(0);
+
+        line.getSubwayMap().add(new Station("교대역"));
+        line.getSubwayMap().add(new Station("강남역"));
+        line.getSubwayMap().add(new Station("역삼역"));
+
+        line = lines.get(1);
+
+        line.getSubwayMap().add(new Station("교대역"));
+        line.getSubwayMap().add(new Station("남부터미널역"));
+        line.getSubwayMap().add(new Station("양재역"));
+        line.getSubwayMap().add(new Station("매봉역"));
+
+        line = lines.get(2);
+
+        line.getSubwayMap().add(new Station("강남역"));
+        line.getSubwayMap().add(new Station("양재역"));
+        line.getSubwayMap().add(new Station("양재시민의숲역"));
+    }
+
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
     }
@@ -18,5 +43,9 @@ public class LineRepository {
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static Line getLines(int index){
+        return lines.get(index);
     }
 }
