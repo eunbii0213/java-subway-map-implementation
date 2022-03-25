232a96561eb9a2d1fc4154cbfd6524a602c02469
eunbii0213 <eunbii0213@naver.com>
2022-03-25 21:43:35 +0900

    refactor: code refactoring

--- src/main/java/subway/SectionView.java
+++ src/main/java/subway/SectionView.java
@@ -4,13 +4,13 @@ public class SectionView extends View{
     private static final String AFTER_SELECT_SECTOR_MANAGE_MESSAGE = "## 구간 관리 화면 ";
     private static final String ADD_SECTOR_GUIDE_MESSAGE = "1. 구간 등록";
     private static final String REMOVE_SECTOR_GUIDE_MESSAGE = "2. 구간 삭제";
-    private static final String SECTOR_INSERT_NAME_GUIDE_MESSAGE = "## 노선을 입력하세요.";
-    private static final String SECTOR_INSERT_STATION_NAME_GUIDE_MESSAGE = "## 역이름을 입력하세요.";
-    private static final String SECTOR_INSERT_NUMBER_GUIDE_MESSAGE = "## 순서를 입력하세요.";
-    private static final String SECTOR_INSERT_COMPLETE_MESSAGE = "구간이 등록되었습니다.";
-    private static final String SECTOR_REMOVE_LINE_GUIDE_MESSAGE = "## 삭제할 노선 이름을 입력하세요.";
-    private static final String SECTOR_REMOVE_STATION_GUIDE_MESSAGE = "## 삭제할 노선 이름을 입력하세요.";
-    private static final String SECTOR_REMOVE_COMPLETE_MESSAGE = "구간이 삭제되었습니다.";
+    private static final String SECTION_INSERT_NAME_GUIDE_MESSAGE = "## 노선을 입력하세요.";
+    private static final String SECTION_INSERT_STATION_NAME_GUIDE_MESSAGE = "## 역이름을 입력하세요.";
+    private static final String SECTION_INSERT_NUMBER_GUIDE_MESSAGE = "## 순서를 입력하세요.";
+    private static final String SECTION_INSERT_COMPLETE_MESSAGE = "구간이 등록되었습니다.";
+    private static final String SECTION_REMOVE_LINE_GUIDE_MESSAGE = "## 삭제할 노선 이름을 입력하세요.";
+    private static final String SECTION_REMOVE_STATION_GUIDE_MESSAGE = "## 삭제할 노선 이름을 입력하세요.";
+    private static final String SECTION_REMOVE_COMPLETE_MESSAGE = "구간이 삭제되었습니다.";
 
     public void showSectorMenuGuide() {
         System.out.println();
@@ -23,36 +23,36 @@ public class SectionView extends View{
 
     public void showSectorInsertGuide() {
         System.out.println();
-        System.out.println(SECTOR_INSERT_NAME_GUIDE_MESSAGE);
+        System.out.println(SECTION_INSERT_NAME_GUIDE_MESSAGE);
     }
 
     public void showSectorInsertStationNameGuide() {
         System.out.println();
-        System.out.println(SECTOR_INSERT_STATION_NAME_GUIDE_MESSAGE);
+        System.out.println(SECTION_INSERT_STATION_NAME_GUIDE_MESSAGE);
     }
 
     public void showSectorInsertNumberGuide() {
         System.out.println();
-        System.out.println(SECTOR_INSERT_NUMBER_GUIDE_MESSAGE);
+        System.out.println(SECTION_INSERT_NUMBER_GUIDE_MESSAGE);
     }
 
     public void showSectorInsertComplete() {
         System.out.println();
-        System.out.println(INFO_MESSAGE + SECTOR_INSERT_COMPLETE_MESSAGE);
+        System.out.println(INFO_MESSAGE + SECTION_INSERT_COMPLETE_MESSAGE);
     }
 
     public void showSectorRemoveLineGuide() {
         System.out.println();
-        System.out.println(SECTOR_REMOVE_LINE_GUIDE_MESSAGE);
+        System.out.println(SECTION_REMOVE_LINE_GUIDE_MESSAGE);
     }
 
     public void showSectorRemoveStationGuide() {
         System.out.println();
-        System.out.println(SECTOR_REMOVE_STATION_GUIDE_MESSAGE);
+        System.out.println(SECTION_REMOVE_STATION_GUIDE_MESSAGE);
     }
 
     public void showSectorRemoveComplete() {
         System.out.println();
-        System.out.println(INFO_MESSAGE + SECTOR_REMOVE_COMPLETE_MESSAGE);
+        System.out.println(INFO_MESSAGE + SECTION_REMOVE_COMPLETE_MESSAGE);
     }
 }
